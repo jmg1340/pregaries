3da83b0c152d43b7b3b3b2752d86b4482238b1c8
jmg1340 <jordi.miserachs@gmail.com>
2022-09-08 07:39:48 +0200

    commit 14

--- src/stores/example-store.js
+++ src/stores/example-store.js
@@ -1,48 +1,104 @@
-import { defineStore } from 'pinia';
+import { defineStore } from "pinia";
 
-export const useAppStore = defineStore('AppStore', {
+export const useAppStore = defineStore("AppStore", {
   state: () => ({
-		tipusPregaria: null,
-		bToolBar: false,
+    tipusPregaria: null,
+    bToolBar: false,
     tamanyFont: 17,
-		arrFavorits: [],
-		pregariaActiva: null,
+    arrFavorits: [],
+    pregariaActiva: null,
   }),
   getters: {
-		getClauInFavorits: ( state ) => state.arrFavorits.includes( state.pregariaActiva ),
+    getClauInFavorits: (state) =>
+      state.arrFavorits.includes(state.pregariaActiva),
   },
   actions: {
-		setToolBar (sino) { this.bToolBar = sino },
-		
-		setTipusPregaria ( tipus ) { this.tipusPregaria = tipus },
+    /**
+     * Posar la barra d'eines (component tamanyLletra.vue) visible o no.
+     *
+     * @param {boolean} sino
+     */
+    setToolBar(sino) {
+      this.bToolBar = sino;
+    },
 
+    /**
+     * Serveix per si s'ha de mostrar el boto de FAVORITS o no a la barra d'eines.
+     * Les pregàries compostes no tenen visible la icona de favorits.
+     *
+     * @param {string} tipus Pregaria individual o composta (Rosari i Coronilla)
+     */
+    setTipusPregaria(tipus) {
+      this.tipusPregaria = tipus;
+    },
+
+    /**
+     * Augmenta el tamany de la lletra de les pregàries
+     */
     increment() {
       if (this.tamanyFont < 50) this.tamanyFont++;
     },
+
+    /**
+     * Disminueix el tamany de la lletra de les pregàries.
+     */
     decrement() {
       if (this.tamanyFont > 12) this.tamanyFont--;
     },
-		setTamanyFont ( valor ) { this.tamanyFont = valor },
 
+    /**
+     * Estableix el tamany de la lletra de les pregàries.
+     * Utilitzat per recuperar aquesta dada de localStorage
+     *
+     * @param {number} valor
+     */
+    setTamanyFont(valor) {
+      this.tamanyFont = valor;
+    },
 
+    setPregariaActiva(clau) {
+      this.pregariaActiva = clau;
+    },
+    getPregariaActiva() {
+      return this.pregariaActiva;
+    },
 
-		setPregariaActiva ( clau ) { this.pregariaActiva = clau },
-		getPregariaActiva() { return this.pregariaActiva },
+    /**
+     * Afegeix la clau de la oració al array de oracions favorites
+     *
+     * @param {string} clau
+     */
+    afegirFavorit(clau) {
+      this.arrFavorits.push(clau);
+    },
 
+    /**
+     * Treu una clau d'una oració que esta al array de oracions favorites
+     *
+     * @param {string} clau
+     */
+    treureFavorit(clau) {
+      this.arrFavorits.splice(this.arrFavorits.indexOf(clau), 1);
+    },
 
+    /**
+     * Recupera l'array de favorits del store per emmagatzemar-lo al LocalStorage
+     *
+     * @returns
+     */
+    getFavorits() {
+      return this.arrFavorits;
+    },
 
-
-		afegirFavorit (clau) {
-			this.arrFavorits.push(clau)
-		},
-		treureFavorit (clau) {
-			this.arrFavorits.splice(this.arrFavorits.indexOf(clau), 1)
-		},
-		getFavorits() { return this.arrFavorits },
-		setFavorits( arr ) { 
-			if ( !arr === null || !arr=== undefined ) {
-				this.arrFavorits = JSON.parse(arr) 
-			}
-		}
+    /**
+     * Estableix el array de favorits de la dada emmagatzemada al localStorage
+     *
+     * @param {string} arr
+     */
+    setFavorits(arr) {
+      if (!arr === null || !arr === undefined) {
+        this.arrFavorits = JSON.parse(arr);
+      }
+    },
   },
 });
