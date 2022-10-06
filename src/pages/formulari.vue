f387357406d0c90472559c3c3250c397b19ce8bb
jmg1340 <jordi.miserachs@gmail.com>
2022-10-06 22:07:28 +0200

    formulari

--- src/pages/formulari.vue
+++ src/pages/formulari.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="q-pa-md">
 
+    <q-card class="q-mb-md">
+      <q-card-section>
+        Ajuda'ns a ampliar el repertori de pregàries enviant més. També pots fer servir el formulari per fer-nos arribar algun suggeriment sobre l'aplicació. MOLTES GRÀCIES.
+      </q-card-section>
+    </q-card>
+
+
     <q-card class="q-pa-md">
-      <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+      <q-form ref="myForm" @submit.prevent.stop="onSubmit" @reset="onReset" class="q-gutter-md">
         <q-input filled v-model="nom" label="El teu nom *" lazy-rules
           :rules="[ val => val && val.length > 0 || 'Per favor, escriu el teu nom']" />
         
@@ -12,14 +19,14 @@
           val => (val.includes('@') && val.includes('.')) || 'Per favor, escriu un email valid'
           ]" />
 
-        <q-input filled type="textarea" v-model="pregaria" label="Pregària *" lazy-rules :rules="[
+        <q-input filled type="textarea" v-model="pregaria" label="Suggeriment / Pregària *" lazy-rules :rules="[
           val => val && val.length >0  || 'Per favor, escriu la pregaria que vulguis enviar'
         ]" />
 
 
-        <div>
-          <q-btn label="Submit" type="submit" color="primary" />
-          <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+        <div class="row justify-between">
+          <q-btn class="col-auto" label="Enviar" noCaps type="submit" color="brown-10" />
+          <q-btn class="col-auto q-ml-sm" label="Reset" type="reset" color="brown-10" />
         </div>
       </q-form>
     </q-card>
@@ -33,30 +40,117 @@
 <script setup>
   import { useQuasar } from 'quasar'
   import { ref } from 'vue'
+  // import vue from 'vue'
+  import axios from 'axios'
+  // import VueAxios from 'vue-axios'
+
+  // Vue.use(VueAxios, axios)
+
+  // console.log("Vue", vue)
+
 
   const $q = useQuasar()
 
+
+  const myForm = ref(null)
   const nom = ref(null)
   const pregaria = ref(null)
   const email = ref(null)
  
-  const onSubmit = () => {
-    // if (accept.value !== true) {
-    //   $q.notify({
-    //     color: 'red-5',
-    //     textColor: 'white',
-    //     icon: 'warning',
-    //     message: 'You need to accept the license and terms first'
-    //   })
-    // }
-    // else {
-    //   $q.notify({
-    //     color: 'green-4',
-    //     textColor: 'white',
-    //     icon: 'cloud_done',
-    //     message: 'Submitted'
-    //   })
-    // }
+  const onSubmit = async () => {
+    
+    const request = new Request('https://pregaries.bonanova.cat/enviarPregaries.php',
+    {
+      method: 'POST',
+      body: '{"nom":"' + nom.value + '", "email":"' + email.value + '", "pregaria":"' + pregaria.value + '"}'
+    });
+  fetch(request)
+    .then(response => {
+      console.log(response)
+      
+      myForm.value.reset()
+
+      $q.notify({
+        color: 'green-6',
+        textColor: 'white',
+        icon: 'cloud_done',
+        message: 'Suggeriment / pregària ENVIADA'
+      })
+    
+    })
+    .catch(error => {
+      console.error(error)
+      $q.notify({
+        color: 'red-5',
+        textColor: 'white',
+        icon: 'warning',
+        message: 'No hi ha comunicació amb el servidor.\n\n' + error.message
+      })
+
+    });
+
+
+
+
+
+/* 
+    try {
+    let data = new FormData();
+      data.append('nom', nom.value);
+      data.append('email', email.value);
+      data.append('pregaria', pregaria.value);
+
+    const resultat = await axios.post('https://pregaries.bonanova.cat/enviarPregaries.php',
+      data,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        }
+      }
+    );
+
+    console.log("resultat", resultat)
+
+
+
+      // const rawResponse = await fetch('https://pregaries.bonanova.cat/enviarPregaries.php', {
+      //   method: 'POST',
+      //   headers: {
+      //     'Accept': 'application/json',
+      //     'Content-Type': 'application/json'
+      //   },
+      //   body: JSON.stringify({ 
+      //     nom: nom.value, 
+      //     email: email.value,
+      //     pregaria: pregaria.value
+      //    })
+      // });
+  
+      // const content = await rawResponse.json();
+      // console.log("content", content);
+
+      myForm.value.reset()
+
+      $q.notify({
+        color: 'green-6',
+        textColor: 'white',
+        icon: 'cloud_done',
+        message: 'Suggeriment / pregària enviada'
+      })
+      
+
+    } catch (error) {
+      console.log("error:", error);
+      $q.notify({
+        color: 'red-5',
+        textColor: 'white',
+        icon: 'warning',
+        message: 'No hi ha comunicació amb el servidor.\n\n' + error.message
+      })
+      
+    }
+
+ */
   }
 
   const onReset = () => {
@@ -64,6 +158,8 @@
     pregaria.value = null
     email.value = null
   }
+
+
     
 
 
