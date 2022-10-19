e81b5a749af73ed1e0c8deb91e47a828489981a0
jmg1340 <jordi.miserachs@gmail.com>
2022-10-19 21:50:53 +0200

    categories expansionables

--- src/components/categories.vue
+++ src/components/categories.vue
@@ -1,20 +1,61 @@
 <template>
   <q-card>
+
+		<div class="column items-end">
+			<div class="row q-gutter-xs q-pa-xs">
+				<div class="col-auto">
+					<q-btn @click="replegarDesplegar(true)" dense color="brown-10" icon="keyboard_double_arrow_down" />
+				</div>
+				<div class="col-auto">
+					<q-btn @click="replegarDesplegar(false)" dense color="brown-10" icon="keyboard_double_arrow_up" />
+				</div>
+			</div>
+		</div>
+
+		
     <q-card-section>
-			<div v-for="(arr, categoria) in objCategories" :key="categoria" class="q-my-md">
+
+			<q-expansion-item
+				v-for="(arr, categoria) in objCategories" :key="categoria"
+				v-model="objModel[categoria]"
+				class="q-mb-md shadow-3 overflow-hidden"
+				style="border-radius: 30px"
+				icon="folder"
+				dense
+				:label="categoria"
+				:header-class="{'bg-red-10 text-white text-bold': objModel[categoria], 'text-red-10 text-bol': !objModel[categoria]}"
+				
+				expand-icon-class="text-red-10"
+			>
+
+					<q-list v-for="(obj, index) in arr" :key="index" bordered separator dense>
+						<q-item clickable v-ripple class="bg-yellow-1" :to="'/pregaria/' + obj.clau">{{obj.titol}}</q-item>
+					</q-list>
+
+
+
+			</q-expansion-item>
+
+			<!-- <div 
+				v-for="(arr, categoria) in objCategories" :key="categoria" class="q-my-md">
 				<div>
 					<div class="text-red-10 text-bold ">{{ categoria }}</div>
 					<q-list v-for="(obj, index) in arr" :key="index" bordered separator dense>
-						<q-item clickable v-ripple class="bg-yellow-2" :to="'/pregaria/' + obj.clau">{{obj.titol}}</q-item>
+						<q-item clickable v-ripple class="bg-yellow-1" :to="'/pregaria/' + obj.clau">{{obj.titol}}</q-item>
 					</q-list>
 				</div>
-			</div>
+			</div> -->
     </q-card-section>
   </q-card>
 </template>
 
 <script setup>
 	import objOracions from "../dades/oracions.json";
+	import { ref, toRef, toRefs, isRef, watch, onMounted, computed } from 'vue'
+	import { useQuasar } from 'quasar'
+	import _ from "lodash";
+
+	const $q = useQuasar()
 
 
 	/* Objectiu: array de objectes tipus:
@@ -25,38 +66,134 @@
 			}
 	*/
 
-	let objCategories = {}
-	
-	/**
-	 * 1. Array de les claus del objecte Oracions
-	 */
-	const arrClausOracions = Object.keys(objOracions)
 
 
-	/**
-	 * 2. Obtencio array de totes les categories
-	 */
-	const arrCatClauTitol = []
-	let arrCategories = []
-	arrClausOracions.forEach( (clau) => {
+	const arrClausOracions = computed(() => Object.keys(objOracions))
+	const arrCategories = computed(() => {
 
-		// generacio de 'arrCategories'
-		objOracions[clau].categories.forEach( (c) =>{
-			if (! arrCategories.includes( c )) arrCategories.push( c )
+		/**
+		 * 1. Array de les claus del objecte Oracions
+		 */
+		// const arrClausOracions = Object.keys(objOracions)
+
+
+		/**
+		 * 2. Obtencio array de totes les categories
+		 */
+		let arrCategories2 = []
+		arrClausOracions.value.forEach( (clau) => {
+
+			// generacio de 'arrCategories'
+			objOracions[clau].categories.forEach( (c) =>{
+				if (! arrCategories2.includes( c )) arrCategories2.push( c )
+			})
+
 		})
-		arrCategories.sort()
+
+		return arrCategories2.sort()
+
+	})
+
+	console.log("arrCategories COMPUTED:", arrCategories.value);
+
+
+	const arrCatClauTitol = computed(() => {
+
+		/**
+		 * 1. Array de les claus del objecte Oracions
+		 */
+		// const arrClausOracions = Object.keys(objOracions)
+
+
+
+		const arrCatClauTitol2 = []
+		arrClausOracions.value.forEach((clau) => {
+
+			// generacio de 'arrCatClauTitol2'
+			arrCatClauTitol2.push({ categories: objOracions[clau].categories, clau: clau, titol: objOracions[clau].titol })
+		})
+
+
+		// console.log("objCategories", objCategories)
+		return arrCatClauTitol2
+
+	})
+
+	console.log("arrCatClauTitol COMPUTED:", arrCatClauTitol.value);
+
+
+
+	const objCategories = computed(() => {
+
+		let objCategories2 = {}
+		arrCategories.value.forEach((cat) => {
+			objCategories2[cat] = arrCatClauTitol.value.filter(obj => obj.categories.includes(cat)).map(obj => ({ clau: obj.clau, titol: obj.titol }))
+		})
+
+		return objCategories2
 		
-		// generacio de 'arrCatClauTitol'
-		arrCatClauTitol.push(  {categories: objOracions[clau].categories, clau: clau, titol: objOracions[clau].titol} )
 	})
 
-	
-	arrCategories.forEach( (cat) => {
-		objCategories[cat] = arrCatClauTitol.filter ( obj => obj.categories.includes(cat) && obj.titol !== '' ).map (obj => ({clau: obj.clau, titol: obj.titol}))
-	})
-	
 
-	// console.log("objCategories", objCategories)
+
+
+
+
+
+	// arrCategories.forEach(key => { 	objModel.value[key] = true })
+/* 	
+	const posarPropietatsReactives = () => {
+		arrCategories.forEach(key => {
+			// objModel.value[key] = true
+			toRef( objModel, key )
+			
+			if (isRef(objModel.value[key])) {
+				console.log("objModelvalue[key] SI REACTIU");
+			} else {
+				console.log("objModelvalue[key] NO REACTIU")
+			}
+			
+		});
+	}
+ 
+	posarPropietatsReactives()
+*/	
+	let objModel = ref({})
+
+	const replegarDesplegar = (trueFalse) => {
+		for (let cat in objModel.value) {
+			// console.log("cat", cat);
+			// toRef(objModel.value, key)
+			console.log("es reactiu: " + isRef(objModel.value[cat]))
+			objModel.value[cat] = trueFalse
+		}
+		console.log("objModel", objModel.value)
+	}
+
+
+
+	watch( () => _.cloneDeep(objModel.value), async (newObj, oldObj) => {
+		// guarda a localStorage la variable 'Obj'
+		console.log("--- WATCH ---")
+		$q.localStorage.set("objCategories", JSON.stringify(newObj) )
+	})
+
+
+
+
+	onMounted(() => {
+		// recupera de localStorage el valor de la variable 'objModel'
+		console.log("------- ONMOUNTED ------")
+		if ($q.localStorage.getItem('objCategories') !== null ) {
+			objModel.value = JSON.parse( $q.localStorage.getItem('objCategories') )
+			console.log("objModel ONMOUNTED", objModel)
+		} else {
+			arrCategories.value.forEach( key => { objModel.value[key] = true })
+		}
+
+		arrCategories.value.forEach( key => { toRef(objModel.value, key) })
+	})
+
 
 
 
